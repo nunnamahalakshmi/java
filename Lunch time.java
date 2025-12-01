5695e66edc8e36292456727a0a55bb7716a46803
nunnamahalakshmi <mahalakshminunna45@gmail.com>
2025-12-01 10:35:50 +0530

    Create Lunch time.java

---
+++ Lunch time.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int x = sc.nextInt();
+            if (x >= 1 && x <= 4) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+	}
+}
