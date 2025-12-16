35a43c6e6281332784fc79bfd962311d4b022d56
nunnamahalakshmi <mahalakshminunna45@gmail.com>
2025-12-16 21:11:22 +0530

    Create credit score.java

---
+++ credit score.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		if(x >= 750) 
+		   System.out.println("YES");
+		else 
+		   System.out.println("NO");
+
+	}
+}
